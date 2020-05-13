3c9679b02e5ce3e0f95d710ea215c59fb44776c0
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2020-05-13 21:30:42 +0300

    Add contents of recipe view

--- app/src/main/java/com/example/wattoit/RecipeViewActivity.kt
+++ app/src/main/java/com/example/wattoit/RecipeViewActivity.kt
@@ -1,10 +1,12 @@
 package com.example.wattoit
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.text.util.Linkify
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
 import kotlinx.android.synthetic.main.activity_recipe_view.*
+import kotlinx.android.synthetic.main.content_recipe_view.*
 
 class RecipeViewActivity : AppCompatActivity() {
 
@@ -13,9 +15,32 @@ class RecipeViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_recipe_view)
         setSupportActionBar(toolbar)
 
-        favoriteButton.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        recipeTitle.text = intent.getStringExtra("TITLE")
+
+        recipeLink.text = intent.getStringExtra("LINK")
+        Linkify.addLinks(recipeLink, Linkify.WEB_URLS)
+
+        fillListView(recipeDietLabels, intent.getCharSequenceArrayExtra("DIET_LABELS"))
+
+        fillListView(recipeIngredients, intent.getCharSequenceArrayExtra("INGREDIENTS"))
+
+        favoriteButton.setOnClickListener {
+            TODO("Store current one as liked recipe")
+        }
+    }
+
+    private fun fillListView(view: LinearLayout, dietLabels: Array<CharSequence>) {
+        if (dietLabels.isNotEmpty()) {
+            view.removeAllViews()
+            for (label in dietLabels) {
+                val tv = TextView(applicationContext).apply {
+                    layoutParams = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT)
+                    text = label
+                }
+                view.addView(tv)
+            }
         }
     }
 
