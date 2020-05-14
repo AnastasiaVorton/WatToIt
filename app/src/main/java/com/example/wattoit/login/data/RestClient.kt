fc062eafb94a239766b26fff2bf5ef2aa2dfbbe0
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2020-05-14 12:37:52 +0300

    Fix RestClient interface and its services

--- app/src/main/java/com/example/wattoit/login/data/RestClient.kt
+++ app/src/main/java/com/example/wattoit/login/data/RestClient.kt
@@ -4,34 +4,40 @@ import android.content.Context
 import com.example.wattoit.BuildConfig
 import com.example.wattoit.data.AuthInterceptor
 import com.example.wattoit.data.RecipeService
+import com.example.wattoit.data.SearchService
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RestClient {
     private lateinit var apiService: RecipeService
+    private lateinit var searchService: SearchService
 
     fun getApiService(context: Context): RecipeService {
-
         if (!::apiService.isInitialized) {
-            val retrofit = Retrofit.Builder()
-                .baseUrl(BuildConfig.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(okhttpClient(context))
-                .build()
-
-            apiService = retrofit.create(RecipeService::class.java)
+            apiService = buildRetrofit(context).create(RecipeService::class.java)
         }
-
         return apiService
     }
 
-    /**
-     * Initialize OkhttpClient with our interceptor
-     */
-    private fun okhttpClient(context: Context): OkHttpClient {
-        return OkHttpClient.Builder()
-            .addInterceptor(AuthInterceptor(context))
-            .build()
+    fun getSearchService(context: Context): SearchService {
+        if (!::searchService.isInitialized) {
+            searchService = buildRetrofit(context).create(SearchService::class.java)
+        }
+        return searchService
     }
+
+    private fun buildRetrofit(context: Context): Retrofit = Retrofit.Builder().apply {
+        baseUrl(BuildConfig.BASE_URL)
+        addConverterFactory(GsonConverterFactory.create())
+        client(buildOkHttpClient(context))
+    }.build()
+
+    private fun buildOkHttpClient(context: Context): OkHttpClient = OkHttpClient.Builder().apply {
+        connectTimeout(60, TimeUnit.SECONDS)
+        readTimeout(60, TimeUnit.SECONDS)
+        writeTimeout(60, TimeUnit.SECONDS)
+        addInterceptor(AuthInterceptor(context))
+    }.build()
 }
