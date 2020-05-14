9161aaa0799593d119a66d9c015555c63c9905ae
LanaGA <l.galieva@innopolis.ru>
2020-05-14 20:54:30 +0300

    connected favs

--- app/src/main/java/com/example/wattoit/main/ui/fav/FavFragment.kt
+++ app/src/main/java/com/example/wattoit/main/ui/fav/FavFragment.kt
@@ -8,10 +8,23 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wattoit.R
+import com.example.wattoit.data.localDB.RecipeDatabase
+import com.example.wattoit.domain.entity.Recipe
+import com.example.wattoit.login.data.RestClient
+import com.example.wattoit.main.ui.search.MyItemOnClickListener
+import com.example.wattoit.main.ui.search.RecipeAdapter
+import kotlinx.android.synthetic.main.activity_search.*
+import kotlinx.android.synthetic.main.activity_search.view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class FavFragment : Fragment() {
     private lateinit var favViewModel: FavViewModel
+    lateinit var restClient: RestClient
+    lateinit var adapter: RecipeAdapter
+    lateinit var recipeDatabase: RecipeDatabase
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,7 +33,30 @@ class FavFragment : Fragment() {
     ): View? {
         favViewModel =
             ViewModelProviders.of(this).get(FavViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_fav, container, false)
+        restClient = RestClient()
+        recipeDatabase = RecipeDatabase.getInstance(this@FavFragment.requireContext())
+        val root = inflater.inflate(R.layout.fragment_fav, container, false).apply {
+            GlobalScope.launch {
+                val all = recipeDatabase.recipeDao().getSaved()
+                val clickListener: MyItemOnClickListener = object :
+                    MyItemOnClickListener {
+                    override fun onClick(recipe: Recipe) {
+                        println("hi babe")
+                    }
+                }
+
+                requireActivity().runOnUiThread {
+                    adapter = RecipeAdapter(
+                        all,
+                        clickListener
+                    )
+                    val lManager = LinearLayoutManager(this@FavFragment.requireContext())
+                    recipiesRView.layoutManager = lManager
+                    recipiesRView.adapter = adapter
+                }
+
+            }
+        }
         val textView: TextView = root.findViewById(R.id.text_home)
         favViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
